57df7a49fb405b1c1e663d6de6ae348affaedb22
leewillis77 <leewillis77@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 15:39:43 +0000

    Tagging version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/github-embed/tags/1.4@976110 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- github-embed.php
+++ github-embed.php
@@ -4,7 +4,7 @@
 Plugin Name: Github Embed
 Plugin URI: http://www.leewillis.co.uk/wordpress-plugins
 Description: Paste the URL to a Github project into your posts or pages, and have the project information pulled in and displayed automatically
-Version: 1.3
+Version: 1.4
 Author: Lee Willis
 Author URI: http://www.leewillis.co.uk/
 */
@@ -252,16 +252,12 @@ class github_embed {
 
 		foreach ( $contributors as $contributor ) {
 
-			$details = $this->api->get_user ($contributor->login);
 			$response->html .= '<li class="github-repo-contributor">';
 			$response->html .= '<img class="github-repo-contributor-avatar" src="';
-			$response->html .= esc_url(add_query_arg(array('s'=>$gravatar_size), $contributor->avatar_url));
-			$response->html .= '" alt="Picture of '.esc_attr($contributor->login).'">';
-			if ( ! empty ( $details->name ) ) {
-				$response->html .= '<span class="github-repo-contributor-name">'.esc_html($details->name)."</span><br>";
-			}
+			$response->html .= esc_url(add_query_arg(array('s'=>$gravatar_size), $contributor->author->avatar_url));
+			$response->html .= '" alt="Picture of '.esc_attr($contributor->author->login).'">';
 			$response->html .= '<span class="github-repo-contributor-login">';
-			$response->html .= '<a href="https://github.com/'.esc_attr($contributor->login).'">'.esc_attr($contributor->login).'</a></span>';
+			$response->html .= '<a href="https://github.com/'.esc_attr($contributor->author->login).'">'.esc_attr($contributor->author->login).'</a></span>';
 		}
 
 		$response->html .= '</ul>';
